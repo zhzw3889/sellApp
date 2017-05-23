9ce93b85afa4841b978ccec4856baa071cab8158
John Zhu <hitzhzw3889@126.com>
2017-05-23 15:44:51 +0800

    border-1px

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import goods from './components/goods/goods';
 import ratings from './components/ratings/ratings';
 import seller from './components/seller/seller';
 
+import './common/stylus/index.styl';
+
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
