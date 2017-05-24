639339d5e84d606f0b07be6ad4153ea9d45b0d57
John Zhu <hitzhzw3889@126.com>
2017-05-24 11:24:40 +0800

    header/Pop-up layer/Sticky Footer

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -18,17 +18,27 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper">
+    <div class="bulletin-wrapper"  @click="showDetail">
       <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
     </div>
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%"/>
+    </div>
+    <div v-show="detailShow" class="detail">
+      <div class="detail-wrapper clearfix" >
+        <div class="detail-main">
+
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -40,17 +50,28 @@
        type: Object
      }
    },
+   data() {
+     return {
+       detailShow: false
+     };
+   },
+   methods: {
+     showDetail() {
+       this.detailShow = true;
+     }
+   },
    created() {
      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
    }
  };
- </script>
+</script>
 
- <style lang="stylus" rel="stylesheet/stylus">
+<style lang="stylus" rel="stylesheet/stylus">
   @import "../../common/stylus/mixin.styl"
 
   .header
     position: relative
+    overflow: hidden
     color: #fff
     background: rgba(7, 17, 27, 0.5)
     .content-wrapper
@@ -158,4 +179,25 @@
       height: 100%
       z-index: -1
       filter: blur(10px)/* 模糊效果 */
- </style>
+    .detail
+      position: fixed
+      z-index: 100
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      overflow: auto
+      background: rgba(7, 17, 27, 0.8)
+      .detail-wrapper
+        min-height: 100%        //把X撑到底部
+        .detail-main
+          margin-top: 64px
+          padding-bottom: 64px  //必须, 留下空白用来显示X
+      .detail-close
+        position: relative      //?
+        width: 32px
+        height: 32px
+        margin: -64px auto 0 auto
+        clear: both
+        font-size: 32px
+</style>
