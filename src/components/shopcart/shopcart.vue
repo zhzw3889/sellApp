300eb0826e52aed0c5618fc6833da407bf1d81d3
John Zhu <hitzhzw3889@126.com>
2017-05-31 18:29:19 +0800

    shopcart/list-mask

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,57 +1,62 @@
 <template>
-  <div class="shopcart">
-    <div class="content" @click="toggleList">
-      <div class="content-left">
-        <div class="logo-wrapper">
-        <div class="logo" :class="{'highlight': totalCount>0}">
-        <i class="icon-shopping_cart" :class="{'highlight': totalCount>0}"></i>
+  <div>
+    <div class="shopcart">
+      <div class="content" @click="toggleList">
+        <div class="content-left">
+          <div class="logo-wrapper">
+            <div class="logo" :class="{'highlight': totalCount>0}">
+              <i class="icon-shopping_cart" :class="{'highlight': totalCount>0}"></i>
+            </div>
+            <div class="num" v-show="totalCount>0">
+              {{totalCount}}
+            </div>
           </div>
-        <div class="num" v-show="totalCount>0">
-        {{totalCount}}
+          <div class="price"  :class="{'highlight': totalPrice>0}">
+            ￥{{totalPrice}}
+          </div>
+          <div class="desc">
+            另需配送费￥{{deliveryPrice}}元
           </div>
         </div>
-        <div class="price"  :class="{'highlight': totalPrice>0}">
-        ￥{{totalPrice}}
-        </div>
-        <div class="desc">
-        另需配送费￥{{deliveryPrice}}元
+        <div class="content-right">
+          <div class="pay" :class="payClass">
+            {{payDesc}}
+          </div>
         </div>
       </div>
-      <div class="content-right">
-      <div class="pay" :class="payClass">
-      {{payDesc}}
+      <div class="ball-container">
+        <div v-for="ball in balls">
+          <transition name="drop" @before-enter="beforeDrop" @enter="dropping" @after-enter="afterDrop">
+            <div class="ball" v-show="ball.show">
+              <div class="inner inner-hook"></div>
+            </div>
+          </transition>
         </div>
       </div>
+      <transition name="fold">
+        <div class="shopcart-list" v-show="listShow">
+          <div class="list-header">
+            <h1 class="title">购物车</h1>
+            <span class="empty" @click="empty">清空</span>
+          </div>
+          <div class="list-content" ref="listContent">
+            <ul>
+              <li class="food" v-for="food in selectFoods">
+                <span class="name">{{food.name}}</span>
+                <div class="price">
+                  <span>￥{{food.price*food.count}}</span>
+                </div>
+                <div class="cartcontrol-wrapper">
+                  <cartcontrol @add="addFood" :food="food"></cartcontrol>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </transition>
     </div>
-    <div class="ball-container">
-      <div v-for="ball in balls">
-        <transition name="drop" @before-enter="beforeDrop" @enter="dropping" @after-enter="afterDrop">
-          <div class="ball" v-show="ball.show">
-      <div class="inner inner-hook"></div>
-          </div>
-        </transition>
-      </div>
-    </div>
-    <transition name="fold">
-      <div class="shopcart-list" v-show="listShow">
-        <div class="list-header">
-          <h1 class="title">购物车</h1>
-          <span class="empty" @click="empty">清空</span>
-        </div>
-        <div class="list-content" ref="listContent">
-          <ul>
-            <li class="food" v-for="food in selectFoods">
-              <span class="name">{{food.name}}</span>
-              <div class="price">
-                <span>￥{{food.price*food.count}}</span>
-              </div>
-              <div class="cartcontrol-wrapper">
-                <cartcontrol @add="addFood" :food="food"></cartcontrol>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
+    <transition name="fade">
+      <div class="list-mask" @click="hideList" v-show="listShow"></div>
     </transition>
   </div>
 </template>
@@ -183,6 +188,9 @@
        this.selectFoods.forEach((food) => {
          food.count = 0;
        });
+     },
+     hideList() {
+       this.fold = true;
      },
      beforeDrop(el) {
        let count = this.balls.length;
@@ -380,4 +388,19 @@
            position: absolute
            right: 0
            bottom: 6px
+ .list-mask
+   position: fixed
+   top: 0
+   left: 0
+   width: 100%
+   height: 100%
+   z-index: 40
+   backdrop-filter: blur(10px)
+   opacity: 1
+   background: rgba(7, 17, 27, 0.6)
+   &.fade-enter-active, &.fade-leave-active
+     transition: all 0.5s
+   &.fade-enter, &.fade-leave-active
+     opacity: 0
+     background: rgba(7, 17, 27, 0)
 </style>
